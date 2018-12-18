698c0fa429aabc9f5ba4f1bf86c522706ae58e9f
Mathieu Morand <mathieu.morand.ch@gmail.com>
2018-12-18 11:16:22 +0100

    - Build : Webpack
    Utiliser correctement les dépendances,
    Build un module avec webpack pour ne plus avoir besoin de require toutes les dépendances dans le projet.
    
    Resolves: [DISF-1268]

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+
+let basePath = path.join(__dirname, '/');
+
+let config = {
+  // Entry, file to be bundled
+  entry: {
+    'session': basePath +  '/lib/session.js'
+  },
+  devtool: 'source-map',
+  output: {
+    // Output directory
+    path: basePath +  '/dist/',
+    library: '[name]',
+    // [hash:6] with add a SHA based on file changes if the env is build
+    filename: '[name].min.js',
+    libraryTarget: 'amd',
+    umdNamedDefine: true
+  },
+  mode: 'production',
+  resolve: {
+    alias: {
+      "async":                basePath + "/node_modules/async/dist/async",
+      "bignum":               basePath + "/lib/bignum",
+      "cryptojs":             basePath + "/node_modules/crypto-js/index",
+      "jquery":               basePath + "/node_modules/jquery/dist/jquery",
+      "otp":                  basePath + "/lib/otp",
+      "session":              basePath + "/lib/session",
+      "srp":                  basePath + "/lib/srp"
+    },
+    extensions: ['.js'] // File types
+  },
+  plugins: [
+  ]
+};
+
+module.exports = config;
