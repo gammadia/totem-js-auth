598d859c340c35cc0256b5cdb031e20cd05f785e
Mathieu Morand <mathieu.morand.ch@gmail.com>
2018-12-19 11:02:22 +0100

    - Build : Webpack
    Mise à jour de la config webpack pour calculer les chemins correctement depuis le module et non plus depuis la racine.
    
    Resolves: [DISF-1268]

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 const path = require('path');
-
-let basePath = path.join(__dirname, '/');
 let config = {
   // Entry, file to be bundled
-  entry: {
-    'session': basePath +  '/lib/session.js'
-  },
+  entry: path.resolve(__dirname, 'lib/session.js'),
   devtool: 'source-map',
   output: {
     // Output directory
-    path: basePath +  '/dist/',
+    path: path.join(__dirname, 'dist/'),
     library: '[name]',
     // [hash:6] with add a SHA based on file changes if the env is build
     filename: '[name].min.js',
@@ -19,13 +15,13 @@ let config = {
   mode: 'production',
   resolve: {
     alias: {
-      "async":                basePath + "/node_modules/async/dist/async",
-      "bignum":               basePath + "/lib/bignum",
-      "cryptojs":             basePath + "/node_modules/crypto-js/index",
-      "jquery":               basePath + "/node_modules/jquery/dist/jquery",
-      "otp":                  basePath + "/lib/otp",
-      "session":              basePath + "/lib/session",
-      "srp":                  basePath + "/lib/srp"
+      "async":                path.join(__dirname, "node_modules/async/dist/async"),
+      "bignum":               path.join(__dirname, "lib/bignum"),
+      "cryptojs":             path.join(__dirname, "node_modules/crypto-js/index"),
+      "jquery":               path.join(__dirname, "node_modules/jquery/dist/jquery"),
+      "otp":                  path.join(__dirname, "lib/otp"),
+      "session":              path.join(__dirname, "lib/session"),
+      "srp":                  path.join(__dirname, "lib/srp")
     },
     extensions: ['.js'] // File types
   }
