2f3df9a5d69d96078cd1b4801db4aa21ee5ff8ae
Mathieu Morand <mathieu.morand.ch@gmail.com>
2018-12-19 15:01:18 +0100

    - Build : Webpack
    Rollback sur les dépendances gitées,
    Lorsqu'on build le module en postinstall, ses propres dépendances ne sont pas encore chargée et font planter le build.
    
    Resolves: [DISF-1268]

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,12 @@
 const path = require('path');
-console.log([
-  __dirname,
-  path.resolve(__dirname, 'lib/session.js'),
-  path.join(__dirname, "node_modules/crypto-js/index"),
-  path.join(__dirname, "node_modules/jquery/dist/jquery"),
-  path.join(__dirname, "lib/otp"),
-  __dirname
-]);
 let config = {
-  // Entry, file to be bundled
-  entry: path.resolve(__dirname, 'lib/session.js'),
+  entry: {
+    'session': path.resolve('lib/session.js')
+  },
   devtool: 'source-map',
   output: {
-    // Output directory
-    path: path.join(__dirname, 'dist/'),
+    path: path.resolve('dist/'),
     library: '[name]',
-    // [hash:6] with add a SHA based on file changes if the env is build
     filename: '[name].min.js',
     libraryTarget: 'amd',
     umdNamedDefine: true
@@ -23,13 +14,13 @@ let config = {
   mode: 'production',
   resolve: {
     alias: {
-      "async":                path.join(__dirname, "node_modules/async/dist/async"),
-      "bignum":               path.join(__dirname, "lib/bignum"),
-      "cryptojs":             path.join(__dirname, "node_modules/crypto-js/index"),
-      "jquery":               path.join(__dirname, "node_modules/jquery/dist/jquery"),
-      "otp":                  path.join(__dirname, "lib/otp"),
-      "session":              path.join(__dirname, "lib/session"),
-      "srp":                  path.join(__dirname, "lib/srp")
+      "async":                path.resolve("node_modules/async/dist/async.js"),
+      "bignum":               path.resolve("lib/bignum.js"),
+      "cryptojs":             path.resolve("node_modules/crypto-js/index.js"),
+      "jquery":               path.resolve("node_modules/jquery/dist/jquery.js"),
+      "otp":                  path.resolve("lib/otp.js"),
+      "session":              path.resolve("lib/session.js"),
+      "srp":                  path.resolve("lib/srp.js")
     },
     extensions: ['.js'] // File types
   }
