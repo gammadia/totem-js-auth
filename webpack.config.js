e8fad5910a7fd17990ebc5ce97808d323aa7329c
Bastien Clément <bastien.clement@gammadia.ch>
2022-02-24 15:24:02 +0100

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 let config = {
-  entry: {
-    'session': path.resolve('lib/session.js')
-  },
-  devtool: 'source-map',
-  output: {
-    path: path.resolve('dist/'),
-    library: '[name]',
-    filename: '[name].min.js',
-    libraryTarget: 'amd',
-    umdNamedDefine: true
-  },
-  mode: 'production',
-  resolve: {
-    alias: {
-      "async":                path.resolve("node_modules/async/dist/async.js"),
-      "bignum":               path.resolve("lib/bignum.js"),
-      "cryptojs":             path.resolve("node_modules/crypto-js/index.js"),
-      "jquery":               path.resolve("node_modules/jquery/dist/jquery.js"),
-      "otp":                  path.resolve("lib/otp.js"),
-      "session":              path.resolve("lib/session.js"),
-      "srp":                  path.resolve("lib/srp.js")
+    entry: {
+        'session': path.resolve('lib/session.js')
     },
-    extensions: ['.js'] // File types
-  }
+    devtool: 'source-map',
+    output: {
+        path: path.resolve('dist/'),
+        library: '[name]',
+        filename: '[name].min.js',
+        libraryTarget: 'amd',
+        umdNamedDefine: true
+    },
+    mode: 'production',
+    resolve: {
+        alias: {
+            "bignum": path.resolve("lib/bignum.js"),
+            "otp": path.resolve("lib/otp.js"),
+            "session": path.resolve("lib/session.js"),
+            "srp": path.resolve("lib/srp.js")
+        },
+        extensions: ['.js'] // File types
+    },
+    externals: {
+        jquery: "jquery"
+    }
 };
 
 module.exports = config;
